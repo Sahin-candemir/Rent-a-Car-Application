92abcc7f0b64817cd65cf5ecd5f4525c20ea29eb
shnca <shncandemir@gmail.com>
2024-08-03 16:21:21 +0300

    Add Swagger integration for API documentation

--- auth-service/auth-service/src/main/java/com/shn/authservice/config/SecurityConfig.java
+++ auth-service/auth-service/src/main/java/com/shn/authservice/config/SecurityConfig.java
@@ -24,6 +24,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/auth-service/swagger-ui/**").permitAll()
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated()
                 )
